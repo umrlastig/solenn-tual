9b0b6e37cec3bd38fe666b562ba3a0c28761b294
solenn-tl <solenn.tual@gmail.com>
2023-06-07 17:04:14 +0200

    Update js

--- js/reviewing.js
+++ js/reviewing.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 
-// hal.js | Copyright (c) 2019 Emmanuel Pietriga (INRIA) & Guillaume Touya (IGN)
+// hal.js | Copyright (c) 2019 IGN
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,129 +20,231 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-const CVIS = {
-    BOOK_LIST: ["Books"],
-    JOUR_LIST: ["IJGIS", "CaGIS", "TiGIS", "JOSIS", "IJGI", "IJC", "Carto", "Other GIS", "SpatialCog", "IEEE", "ISPRS"],
-    CONF_LIST: ["ICC", "GIScience", "GeneICA", "SAGEO", "ISPRS", "ISSDQ"],
-    TIME_INTERVAL: ["2006", "2021"],
-    SVG_W: 800,
-    TITLE_Y: 26,
-    CAPTION_X: 40,
-    TRACK_WIDTH: 300,
-    TRACK_HEIGHT: 18,
-    TRACK_X: 110,
-    CELL_SIZE: 12,
-    CELL_STROKE: 2,
-    CELL_MARGIN: 2,
-};
+// documentation on HAL API: https://api.archives-ouvertes.fr/docs/search/?schema=fields#fields
 
-const LEGENDS = {
-    journals: [
-        //{type: "JGED1", desc: "Guest Editor"},
-        {type: "JREW1", desc: "Reviewer - 1 paper"},
-        //{type: "JGED2", desc: "Guest Editor"},
-        {type: "JREW2", desc: "Reviewer - 2 papers"},
-        {type: "JREW3", desc: "Reviewer - 3+ papers"},
-        {type: "JGED3", desc: "Guest Editor"},
-    ],
-    conferences: [
-        {type: "CHAIR", desc: "Chair"},
-        {type: "PCM", desc: "PC Member/Associate Chair"},
-        {type: "CREW", desc: "Reviewer"},
-    ],
-    books: [
-        {type: "BED", desc: "Editor"},
-        {type: "BREW", desc: "Reviewer"},
-    ],
-};
-
-/* ---------------------------------- */
-const timeParser = d3.timeParse("%Y");
-
-// time axis
-var timeScale = d3.scaleTime()
-                  .domain(d3.extent(CVIS.TIME_INTERVAL, (d) => timeParser(d)))
-                  .rangeRound([0, (CVIS.TIME_INTERVAL[1]-CVIS.TIME_INTERVAL[0])*(CVIS.CELL_SIZE+CVIS.CELL_STROKE+2*CVIS.CELL_MARGIN)]);
-
-/* ---------------------------------- */
-
-var _reviewing4x = function(data, venueList, jobList, parentEl, svg_h, heading){
-    var svgEl = d3.select(parentEl).append("svg").classed("rev", true);
-    svgEl.attr("width", CVIS.SVG_W);
-    svgEl.attr("height", svg_h);
-    svgEl.append("text")
-         .classed("venueType", true)
-         .attr("transform", "translate(0,"+CVIS.TITLE_Y+")")
-         .text(heading);
-
-    var legendEl = svgEl.append("g")
-                        .attr("transform",
-                              "translate("+CVIS.CAPTION_X+",54)");
-    legendEl.selectAll("rect.job")
-            .data(jobList)
-            .enter()
-            .append("rect")
-            .classed("job", true)
-            .attr("x", CVIS.TRACK_X+CVIS.TRACK_WIDTH)
-            .attr("y", (d,i) => (i*CVIS.TRACK_HEIGHT-CVIS.CELL_SIZE))
-            .attr("width", CVIS.CELL_SIZE)
-            .attr("height", CVIS.CELL_SIZE)
-            .attr("class", (d) => (d.type));
-    legendEl.selectAll("text.job")
-            .data(jobList)
-            .enter()
-            .append("text")
-            .classed("job", true)
-            .attr("x", CVIS.TRACK_X+CVIS.TRACK_WIDTH+CVIS.CELL_SIZE+4)
-            .attr("y", (d,i) => (i*CVIS.TRACK_HEIGHT))
-            .text((d) => (d.desc));
-
-    svgEl.append("g")
-         .attr("id", heading.toLowerCase())
-         .attr("transform", "translate(0,60)");
-    svgEl.append("g")
-         .attr("transform", "translate("+CVIS.TRACK_X+",54)")
-         .call(d3.axisTop(timeScale).tickValues([timeParser("2010"), timeParser("2015"), timeParser("2020")]).tickSizeOuter(0));
-
-    venueGs = d3.select("g#"+heading.toLowerCase())
-                .selectAll("g.venues")
-                .data(venueList)
-                .enter()
-                .append("g")
-                .classed("venues", true)
-                .attr("transform", (d,i) => ("translate("+(CVIS.TRACK_X-10)+","+((i+1)*CVIS.TRACK_HEIGHT)+")"));
-    venueGs.append("text")
-           .attr("text-anchor", "end")
-           .text((d) => (d));
-    venueGs.append("rect")
-           .attr("x", 10)
-           .attr("y", 0)
-           .attr("width", CVIS.TRACK_WIDTH)
-           .attr("height", 1)
-           .attr("fill", "#DDD");
-    var jg = d3.select("g#"+heading.toLowerCase()).append("g")
-               .attr("transform", "translate("+CVIS.TRACK_X+",0)");
-    jg.selectAll("a")
-      .data(data)
-      .enter()
-      .append("a")
-      .attr("xlink:href", (d) => (d.url))
-      .attr("xlink:title", (d) => (d.description))
-      .append("rect")
-      .attr("x", (d) => (timeScale(timeParser(d.year))-(CVIS.CELL_SIZE)/2))
-      .attr("y", (d) => ((venueList.indexOf(d.name)+1)*CVIS.TRACK_HEIGHT-CVIS.CELL_SIZE))
-      .attr("width", CVIS.CELL_SIZE)
-      .attr("height", CVIS.CELL_SIZE)
-      .attr("class", (d) => (d.type))
-      .attr("title", (d) => (d.description));
-};
-
-var reviewingVis = function(dataURL){
-    d3.json(dataURL).then(
-        function(data){
-            _reviewing4x(data.journals, CVIS.JOUR_LIST, LEGENDS.journals, "#revj", 320, "Journals");
-            _reviewing4x(data.conferences, CVIS.CONF_LIST, LEGENDS.conferences, "#revc", 280, "Conferences");
-            _reviewing4x(data.books, CVIS.BOOK_LIST, LEGENDS.books, "#revb", 110, "Books");
-        }
-    ).catch(function(err){console.log(err);});
-};
+var halApi = function(halId){
+    const fl = 'fileAnnexesFigure_s,invitedCommunication_s,proceedings_s,popularLevel_s,halId_s,authIdHalFullName_fs,producedDateY_i,docType_s,files_s,fileMain_s,fileMainAnnex_s,linkExtUrl_s,title_s,en_title_s,fr_title_s,label_bibtex,citationRef_s';
+    return "https://api.archives-ouvertes.fr/search/?q=authIdHal_s:%22"+halId+"%22&wt=json&sort=producedDateY_i desc&rows=10000&fl="+fl;
+  }
+  
+  var getPublications = function(halId, parent, params){
+    if (!parent) return;
+  
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest();
+  
+    // Open a new connection, using the GET request on the URL endpoint
+    var url = halApi(halId)+params;
+    // console.log(url);
+    request.open('GET', url, true);
+    request.onload = function () {
+      var docs = JSON.parse(this.response).response.docs;
+      // console.log(docs);
+      if(docs.length == 0) {
+        parent.hidden = true;
+      } else {
+        const ol = document.createElement('ol');
+        ol.setAttribute("class","sub");
+        docs.forEach(doc => createPub(doc, ol));
+        parent.appendChild(ol);
+      }
+    };
+    request.send();
+  }
+  
+  const publication_options = {
+    pubPV:  "&fq=popularLevel_s:1",
+    pubASCL:"&fq=popularLevel_s:0&fq=docType_s:\"ART\"&fq=peerReviewing_s:0",
+    pubACL: "&fq=popularLevel_s:0&fq=docType_s:\"ART\"&fq=peerReviewing_s:1&fq=audience_s:2",
+    pubACLN:"&fq=popularLevel_s:0&fq=docType_s:\"ART\"&fq=peerReviewing_s:1&fq=audience_s:(1 OR 3)",
+    pubINV: "&fq=popularLevel_s:0&fq=docType_s:\"COMM\"&fq=invitedCommunication_s:1",
+    pubCOM: "&fq=popularLevel_s:0&fq=docType_s:\"COMM\"&fq=invitedCommunication_s:0&fq=proceedings_s:0",
+    pubACTI:"&fq=popularLevel_s:0&fq=docType_s:\"COMM\"&fq=invitedCommunication_s:0&fq=proceedings_s:1&fq=audience_s:2",
+    pubACTN:"&fq=popularLevel_s:0&fq=docType_s:\"COMM\"&fq=invitedCommunication_s:0&fq=proceedings_s:1&fq=audience_s:(1 OR 3)",
+    pubOS:  "&fq=popularLevel_s:0&fq=docType_s:\"COUV\"",
+    pubDO:  "&fq=popularLevel_s:0&fq=docType_s:\"DOUV\"",
+    pubAP:  "&fq=popularLevel_s:0&fq=docType_s:(\"REPORT\" OR \"UNDEFINED\")",
+    pubTH:  "&fq=popularLevel_s:0&fq=docType_s:(\"THESE\" OR \"HDR\")",
+    pubAFF: "&fq=popularLevel_s:0&fq=docType_s:\"POSTER\""
+  }
+  
+  // based on http://production-scientifique.bnf.fr/Annexe/cadre-de-classement
+  function classement(doc) 
+  {
+    if (doc.popularLevel_s == 1) return 'PV';
+    if (doc.docType_s == 'COUV') return 'OS';
+    if (doc.docType_s == 'DOUV') return 'DO';
+    if (doc.docType_s == 'POSTER') return 'AFF';
+    if (doc.docType_s == 'THESE' || doc.docType_s == 'HDR') return 'TH';
+    if (doc.docType_s == 'REPORT' || doc.docType_s == 'UNDEFINED') return 'AP';
+    if (doc.docType_s == 'COMM')
+    {
+      console.log(doc);
+      if (doc.invitedCommunication_s == 1) return 'INV';
+      if (doc.proceedings_s == 0) return 'COM';
+      if (doc.audience_s == 2) return 'ACTI';
+      return 'ACTN';
+    }
+    if (doc.docType_s == 'ART')
+    {
+      if (doc.peerReviewing_s == 0) return 'ASCL';
+      if (doc.audience_s == 2) return 'ACL';
+      return 'ACLN';
+    }
+    return '???';
+  }
+  
+  var getPublicationsAuthor = function(halId, options = publication_options)
+  {
+    for (var id in options)
+      getPublications(halId, document.getElementById(id), options[id]);
+  }
+  
+  var getKeywordPublicationsAuthor = function(halId, keyword, parent){
+    parent = document.getElementById(parent || "pub") || parent;
+    getPublications(halId, parent, "&fq=keyword_s:\""+keyword+"\"");
+  }
+  
+  var getKeywordPublicationsAuthorYear = function(halId, keyword, year, parent){
+    parent = document.getElementById(parent || "pub") || parent;
+    getPublications(halId, parent, "&fq=keyword_s:\""+keyword+"\"&fq=producedDateY_i:"+year);
+  }
+  
+  var getKeywordPublicationsAuthorStartYear = function(halId, keyword, startYear, parent, endYear){
+    parent = document.getElementById(parent || "pub") || parent;
+  console.log(parent);
+    endYear = endYear || new Date().getFullYear();
+    for(year=endYear;year >= startYear;year--)
+    {
+      var divElement = document.createElement('div');
+      divElement.innerHTML = year;
+      const yearParentElement = parent.appendChild(divElement);
+          getKeywordPublicationsAuthorYear(halId, keyword, year, yearParentElement);
+    }
+  }
+  
+  function parseCitation(doc, citationElement, linksElement)
+  {
+    var regex = /. <a[^>]*href="(https?:\/\/([^"\/]*)\/[^"]*)"[^>]*>&#x27E8;([^<]*)&#x27E9;<\/a>/;
+    var citation = doc.citationRef_s;
+    while((matches = regex.exec(citation)) !== null) {
+      const url = matches[1];
+      var host = matches[2];
+      const text = matches[3];
+      citation = citation.replace(matches[0],'');
+      var icons = {
+        'dx.doi.org': 'doi.svg',
+        'www.mdpi.com': 'mdpi.jpg'
+      }
+      const img = "img/icons/"+(icons[host] || "link.svg")
+  
+      const aElement = document.createElement('a');
+      aElement.setAttribute("href",url);
+      aElement.setAttribute("class","imgLink");
+      imgElement = document.createElement('img');
+      imgElement.setAttribute("title",text);
+      imgElement.setAttribute("src", img);
+      imgElement.setAttribute("height","20");
+      imgElement.setAttribute("alt",text);
+      aElement.appendChild(imgElement);
+      linksElement.appendChild(aElement);
+    }
+    citationElement.innerHTML = citation;
+  }
+  
+  var clickBibtex = function(label_bibtex){
+    const input = document.createElement("input");
+    document.body.appendChild(input);
+    input.value = label_bibtex;
+    input.select();
+    document.execCommand("copy"); 
+    document.body.removeChild(input);
+    alert("This bibtex entry should be copied to the clipboard:\n"+label_bibtex);
+  }
+  
+  function createBibtex(label_bibtex, parent)
+  {
+    // create a span element inside the new div
+    const spanElement = document.createElement('span');
+    spanElement.setAttribute("class","bibtex");
+    // create an input element inside the span
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute("type","image");
+    inputElement.setAttribute("class","imgLink");
+    inputElement.setAttribute("src","img/icons/bibtex.jpg");
+    inputElement.setAttribute("alt","Copy BibTeX to clipboard");
+    inputElement.setAttribute("title","Copy BibTeX to clipboard");
+    inputElement.onclick = function() {clickBibtex(label_bibtex);}
+    spanElement.appendChild(inputElement);
+    return spanElement;
+  }
+  
+  var createPub = function(doc, parent){
+    // console.log(doc);
+    if (!parent) return;
+    const listElement = document.createElement('li');
+    listElement.setAttribute("class", "bib");
+    listElement.setAttribute("id", doc.halId_s);
+    const linksElement = document.createElement('span');
+    // listElement.innerHTML = '<b>'+classement(doc)+'</b>';
+  
+    const authors = document.createElement('span');
+    for(var i = 0; i < doc.authIdHalFullName_fs.length; ++i)
+    {
+      const [_idHal, _fullName] = doc.authIdHalFullName_fs[i].split('_FacetSep_');
+      const author = document.createElement(_idHal ? 'a' : 'span');
+      if(_idHal) author.setAttribute("href",'https://cv.archives-ouvertes.fr/'+_idHal);
+      author.setAttribute("id",_idHal);
+      author.setAttribute("class","author");
+      const span = document.createElement('span');
+      span.innerHTML = _fullName;
+      author.appendChild(span);
+      authors.appendChild(author);
+    }
+    listElement.appendChild(authors);
+  
+    const title = document.createElement('a');
+    
+    title.setAttribute("href",'https://hal.archives-ouvertes.fr/'+doc.halId_s);
+    title.setAttribute("class","title");
+    if (doc.en_title_s && doc.fr_title_s) {
+      const title_en = document.createElement('span');
+      const title_fr = document.createElement('span');
+      title_en.setAttribute("class","lang-en");
+      title_fr.setAttribute("class","lang-fr");
+      title_en.innerHTML = doc.en_title_s;
+      title_fr.innerHTML = doc.fr_title_s;
+      title.appendChild(title_en);
+      title.appendChild(title_fr);
+    } else {
+      title.innerHTML = (doc.en_title_s || doc.fr_title_s || doc.title_s);
+    }
+    listElement.appendChild(title);
+  
+    const ref = document.createElement('span');
+    parseCitation(doc, ref, linksElement);
+    listElement.appendChild(ref);
+  
+    // create an a element with the url of the pdf
+    const file = doc.linkExtUrl_s || doc.fileMain_s || doc.fileMainAnnex_s;
+    const files = doc.files_s || (file ? [file] : []);
+    for(var i = 0; i < files.length; ++i)
+    {
+      const file = files[i];
+      pdfElement = document.createElement('a');
+      pdfElement.setAttribute("href",file);
+      pdfElement.setAttribute("class","imgLink");
+      imgPdfElement = document.createElement('img');
+      imgPdfElement.setAttribute("title","pdf");
+      imgPdfElement.setAttribute("src","img/icons/pdf_icon.gif");
+      imgPdfElement.setAttribute("height","20");
+      imgPdfElement.setAttribute("alt","pdf");
+      pdfElement.appendChild(imgPdfElement);
+      linksElement.appendChild(pdfElement);
+    }
+    linksElement.insertBefore(createBibtex(doc.label_bibtex), linksElement.firstChild);
+    listElement.insertBefore(linksElement, listElement.firstChild);
+    parent.appendChild(listElement);
+    jQuery('lang-en').hide();
+  }
+  
